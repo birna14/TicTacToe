225a312ca4002910438419472a6589f0cc10975f
Svanhvit <svanhviti16@ru.is>
2017-10-16 15:27:59 +0000

    nýtt board

--- src/main/java/is/ru/hugb/TicTacToe.java
+++ src/main/java/is/ru/hugb/TicTacToe.java
@@ -1,13 +1,23 @@
 package is.ru.hugb;
 
-public class TicTacToe {
-	public static String hello() {
-		return "Hello World";
-	}
-    public static void main(String[] args) {
-        hello();
-        return;
-    }
+public class Board {
+	private char[][] board;
+	int position;
 
+	public Board(int size) {
+		board = new char[3][3];
+		
+	}
+	
+	//TODO
+	Boolean checkLegalMove() {
+		return true;
+	}
+	
+	//TODO
+	Boolean checkWin();
+	
+	//TODO
+	Boolean isFull();
 }
 
---
+++ src/test/java/is/ru/hugb/TestBoard.java
@@ -0,0 +1,11 @@
+package is.ru.hugb;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class TestBoard {
+        @Test
+        public void testHelloWorld() {
+                assertEquals("Hello World", TicTacToe.hello());
+        }
+}
