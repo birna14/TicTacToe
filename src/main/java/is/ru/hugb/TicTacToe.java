ea3b515dc3de86187036e5749002b65787eeb151
Svanhvit <svanhviti16@ru.is>
2017-10-16 15:50:17 +0000

    bætti við Board.java

--- src/main/java/is/ru/hugb/TicTacToe.java
+++ src/main/java/is/ru/hugb/TicTacToe.java
@@ -1,23 +1,6 @@
 package is.ru.hugb;
 
-public class Board {
-	private char[][] board;
-	int position;
+public class TicTacToe {
 
-	public Board(int size) {
-		board = new char[3][3];
-		
-	}
-	
-	//TODO
-	Boolean checkLegalMove() {
-		return true;
-	}
-	
-	//TODO
-	Boolean checkWin();
-	
-	//TODO
-	Boolean isFull();
 }
 
