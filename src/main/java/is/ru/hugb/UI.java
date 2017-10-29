2398e957c791ce0a54ade215913d77478c72d7b9
birna14 <birna14@ru.is>
2017-10-29 18:42:10 +0000

    Indent fixes and taking out unused code

--- src/main/java/is/ru/hugb/UI.java
+++ src/main/java/is/ru/hugb/UI.java
@@ -6,13 +6,12 @@ import java.net.URLDecoder;
 import static spark.Spark.*;
 
 public class UI {
-
+  // Print board for game in html
   public static String printBoard(Game game) {
-    String out = "<h1>";
+    String out = "<h1>";    
     out += "<table>";
     for (int i = 0; i < 3; i++) {
       out += "<tr>";
-      //out += "<br>";
         for (int j = 0; j < 3; j++) {
           out += "<td>" + game.getBoard()[i][j] + "</td>";
         }
@@ -22,57 +21,46 @@ public class UI {
     return out + "<h1>";
   }
   
+  // Clear board and reset state and player for new game
 	public static void clearBoard(Game game) {
 		for (int i = 0; i < 3; i++) {
-              for (int j = 0; j < 3; j++) {
-                  game.getBoard()[i][j] = ' ';
-              }
-          }
-		  game.resetState();
-	}
-
-
-   private static int validateInput(Scanner scan){
-    while (!scan.hasNextInt()){
-      scan.next();
+      for (int j = 0; j < 3; j++) {
+        game.getBoard()[i][j] = ' ';
+      }
     }
-    return scan.nextInt();
-  }
-
-    // here the game is played
-
-    public static void main(String[] args) {
+		game.resetState();
+	}
+  
+  // Game is played here
+  public static void main(String[] args) {
     // In order for this to work on Heroku, we need to allow Heroku to set the port number
-      staticFileLocation("/public");
-      port(getHerokuPort());
+    staticFileLocation("/public");
+    port(getHerokuPort());
 
-       Game game = new Game();
-       UI ui = new UI();
+    Game game = new Game();
+    UI ui = new UI();
 
-      post("/add", (req, res) -> {
+    post("/add", (req, res) -> {
+      String input = req.queryParams("input");
+      String numbers[] = input.split(",");
 
-           String input = req.queryParams("input");
-           String numbers[] = input.split(",");
-
-			     int iInput1 = Integer.parseInt(numbers[0]);
-		       int iInput2 = Integer.parseInt(numbers[1]);
-
-			game.setCell(iInput1, iInput2);
-
-            if(game.getState() == Game.State.PLAYING){
+			int iInput1 = Integer.parseInt(numbers[0]);
+		  int iInput2 = Integer.parseInt(numbers[1]);
+      
+      game.setCell(iInput1, iInput2);
+        
+      if(game.getState() == Game.State.PLAYING){
 				return ui.printBoard(game) + "<br> <h1>" + game.getPlayer() + "'s turn";
 			}
 			else if(game.getState() == Game.State.WIN) {
-
 				return ui.printBoard(game) + "<br> <h1>" + game.getPlayer() + " has won!! :)";
 			}
 			else {
 				return ui.printBoard(game) + "<br> <h1>It's a draw!";
 			}
-          });
+    });
 
 		delete("/reset", (req, res) -> {
-			//game = new Game();
 			ui.clearBoard(game);
 			return "<h1> Play Tic Tac Toe! </h1>" + ui.printBoard(game) + "<br> <h1>" + game.getPlayer() + "'s turn";
 		});
@@ -81,29 +69,12 @@ public class UI {
       ui.clearBoard(game);
       return "<h1> Play Tic Tac Toe! </h1>" + ui.printBoard(game) + "<br> <h1>" + game.getPlayer() + "'s turn";
     });
-    /*
-    post("/", (req, res) -> {
 
-			if(game.getState() == Game.State.PLAYING){
-            return ui.printBoard(game);
-			}
-    });*/
-
-      get("/add/:numbers", (req, res) -> {
-        String input1 = req.queryParams("input1");
-        String input2 = req.queryParams("input2");
-        int iInput1 = Integer.parseInt(input1);
-        int iInput2 = Integer.parseInt(input2);
-
-        return game.setCell(iInput1, iInput2);
-      });
+    static int getHerokuPort() {
+      ProcessBuilder psb = new ProcessBuilder();
+      if (psb.environment().get("PORT") != null) {
+        return Integer.parseInt(psb.environment().get("PORT"));
       }
-
-      static int getHerokuPort() {
-        ProcessBuilder psb = new ProcessBuilder();
-        if (psb.environment().get("PORT") != null) {
-          return Integer.parseInt(psb.environment().get("PORT"));
-        }
-        return 4567;
-      }
+      return 4567;
+    }
 }
